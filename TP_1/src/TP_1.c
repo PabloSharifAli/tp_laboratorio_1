6e1be2657178e7ba934d9aeab070bda299ab18a8
pablo.s.ali@hotmail.com <pablo.s.ali@hotmail.com>
2022-04-02 19:23:54 -0300

    creacion de la carpeta

---
+++ TP_1/src/TP_1.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : TP_1.c
+ Author      : Ali, Pablo Sharif
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+}
